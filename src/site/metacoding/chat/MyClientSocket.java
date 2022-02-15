81a49b7c81b4af42ea1368b9495fe912457775bd
field <jsw_777@naver.com>
2022-02-15 11:01:21 +0900

    chat [3] 전이중 통신 완료

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -1,6 +1,8 @@
 package site.metacoding.chat;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -10,27 +12,40 @@ public class MyClientSocket {
     Socket socket;
     BufferedWriter writer;
     Scanner sc;
-    private boolean isConnected;
+
+    // 추가(메시지 받는 용도)
+    BufferedReader reader;
 
     public MyClientSocket() {
         try {
+            // 서버로 메시지 보내기
             socket = new Socket("localhost", 1077); // 연결 (IP주소, 포트)
             writer = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
 
-            isConnected = true;
+            // 서버로부터 메시지 받기
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
 
+            // 메시지 받아서 출력
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = reader.readLine();
+                        System.out.println("받은 메시지:" + inputData);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 메시지 보내기
             // 스캐너 달고(반복 x)
             sc = new Scanner(System.in);
             // 키보드로부터 입력 받는 부분(반복)
-            while (isConnected) {
+            while (true) {
                 writer.write(sc.nextLine() + "\n"); // 버퍼에 담고(물을 내려야 내려감)
                 writer.flush();
-
-                if (sc.nextLine().equals("접속종료")) {
-                    isConnected = false;
-                    break;
-                }
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -1,9 +1,12 @@
 package site.metacoding.chat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
@@ -11,7 +14,11 @@ public class MyServerSocket {
     Socket socket; // 메시지 통신
     BufferedReader reader;
 
-    private boolean isConnected;
+    // 추가(클라이언트에게 메시지 보내기)
+    BufferedWriter writer;
+    Scanner sc;
+
+    // private boolean isConnected;
 
     public MyServerSocket() {
         try {
@@ -20,21 +27,35 @@ public class MyServerSocket {
             serverSocket = new ServerSocket(1077);
             System.out.println("서버 소켓 생성됨");
             socket = serverSocket.accept(); // while을 '돌면서' 대기(클이 접속할 때까지)(랜덤 포트로)
+
+            // 메시지 읽기
             reader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()) // socket.get~ : 선
             );
 
-            isConnected = true;
-            // 얘를 while 돌리기
-            while (isConnected) {
+            // 메시지 보내기
+            sc = new Scanner(System.in);
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
+            // 클라이언트로 메시지 보내기
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = sc.nextLine();
+                        writer.write(inputData + "\n");
+                        writer.flush();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 클라이언트로부터 메시지 받아서 출력
+            // 얘를 while 돌리기
+            while (true) {
                 String inputData = reader.readLine();
                 System.out.println("받은 메시지:" + inputData);
                 // System.out.println("클라이언트 연결됨");
-
-                if (inputData.equals("접속종료")) {
-                    isConnected = false;
-                }
             }
         } catch (Exception e) {
             System.out.println("통신 오류 발생:" + e.getMessage());
